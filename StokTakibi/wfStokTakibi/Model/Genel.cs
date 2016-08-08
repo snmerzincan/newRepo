7f263f58b259c40f1234489a1d26193dcf887463
snmerzincan <sinemerzincan_89@hotmail.com>
2016-08-08 14:44:28 +0300

    Stok Takip
    Tekstil firmasının stok takibini yapan bir  masaüstü uygulamasıdır. Ürün
    bilgilerinin tutulduğu, stok giriş ve çıkış işlemlerinin yapılabileceği,
    stokların sorgulanabileceği, var olan ürünler ve stoklar üzerinde
    ekleme, güncelleme, silme işlemlerinin yapılabileceği bir uygulamadır.
    Aynı zamanda raporlamada yapılabilir. Proje, Microsoft Visual Studio ve
    MS SQL Server kullanılarak yapılmıştır.

---
+++ StokTakibi/wfStokTakibi/Model/Genel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfStokTakibi.Model
+{
+    class Genel
+    {
+        public static string connStr = "Data Source=HP; Initial Catalog=Stok; Integrated Security=true";
+        public static string caritipi;
+        public static int carino;
+        public static string cariunvan;
+        public static int urunsayfano = 0;
+    }
+}
